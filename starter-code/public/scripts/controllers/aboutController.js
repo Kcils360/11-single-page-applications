557661e63b499329b4dc6f82b73f2ff5b356315a
Gregory Dukes <kcils360@gmail.com>
2017-08-28 13:50:19 -0700

    configured routs for the app and called the function

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -4,8 +4,14 @@ var app = app || {};
 (function(module) {
   const aboutController = {};
 
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
-
+  // DONE: Define a function that hides all main section elements, and then reveals just the #about section:
+  aboutController.init = function() {
+    $('.main-nav').on('click', '.tab', function() {
+      $('.tab-content').hide();
+      $(`#${$(this).data('content')}`).fadeIn();
+    });
+    $('.main-nav .tab:first').click();
+  };
 
   module.aboutController = aboutController;
 })(app);
